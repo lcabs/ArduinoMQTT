f161b4cf33a6413aba58e6ce33eb640ea1bfffdd
lcabs <39957000+lcabs@users.noreply.github.com>
2023-08-03 16:43:21 -0300

    Create main.ino

---
+++ main.ino
@@ -0,0 +1,108 @@
+/*
+ Reconnecting MQTT example - non-blocking
+
+ This sketch demonstrates how to keep the client connected
+ using a non-blocking reconnect function. If the client loses
+ its connection, it attempts to reconnect every 5 seconds
+ without blocking the main loop.
+
+*/
+
+#include <SPI.h>
+#include <Ethernet.h>
+#include <PubSubClient.h>
+
+// Update these with values suitable for your hardware/network.
+byte mac[]    = {  0x00, 0xAA, 0xBB, 0xCC, 0xDE, 0x02 };
+IPAddress ip(192, 168, 18, 169);
+IPAddress server(192, 168, 18, 168);
+
+int SerialPrint = 1;
+long lastReconnectAttempt = 0;
+
+void callback(char* topic, byte* payload, unsigned int length);
+void subscribetoAll();
+
+void callback(char* topic, byte* payload, unsigned int length) {
+ String payloadmsg;
+  for (int i=0;i<length;i++) {
+    payloadmsg += (char)payload[i];
+    payloadmsg[length] = '\0';
+  }
+ if (SerialPrint == 1) 
+        {
+        Serial.print("topic received = ");
+        Serial.println(topic);
+        Serial.print("payload received = ");
+        Serial.println(payloadmsg);
+        }
+
+/*memcpy(payloadmsg, payload, length);
+  payloadmsg[length] = '\0';
+*/
+  Serial.print("Message received in topic [");
+  Serial.print(topic);
+  Serial.print("] ");
+  Serial.print(payloadmsg);
+
+
+}
+
+EthernetClient ethClient;
+PubSubClient client(ethClient);
+
+boolean reconnect() {
+  if (client.connect("ARDUINO","Arduino","12345")) {
+      Serial.println("Arduino On!");
+      subscribetoAll();
+      client.publish("lcabs1993/arduino","Arduino On!");    // Once connected, publish an announcement...
+
+  }
+  return client.connected();
+}
+
+void subscribetoAll(){
+  Serial.println("Subscribed to all.");
+  client.subscribe("lcabs1993/broadcast");
+  client.subscribe("lcabs1993/arduino");
+//  client.subscribe("lcabs1993/arduino");
+//  client.subscribe("lcabs1993/arduino");
+//  client.subscribe("lcabs1993/arduino");
+//  client.subscribe("lcabs1993/arduino");    
+  Serial.println("Subscribed to all.");
+}
+
+
+
+
+void setup()
+{
+  Serial.begin(9600);
+  Serial.println("Serial on!");
+  client.setServer(server, 1883);
+  client.setCallback(callback);
+
+  Ethernet.begin(mac, ip);
+  delay(1500);
+  lastReconnectAttempt = 0;
+}
+
+void loop()
+{
+  if (!client.connected()) {
+    Serial.println("Client disconnected.");
+    long now = millis();
+    if (now - lastReconnectAttempt > 1000) {
+      lastReconnectAttempt = now;
+      // Attempt to reconnect
+      if (reconnect()) {
+        lastReconnectAttempt = 0;
+      }
+    }
+  } else {
+    // Client connected
+
+    client.loop();
+  }
+
+}
